d075826277724aef7c714167fc5e8a60962dae6a
hammad.siddiqui <hammad.siddiqui@querplex.de>
2021-01-19 18:35:30 +0100

    some changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function makeItHeader(str){
-    // if(typeof string !== "string") throw new TypeError("make it header wants a string");
-    return <h2>{str}</h2>;
+    if(typeof string !== "string") throw new TypeError("make it header wants a string");
+    return `****${str}****`;
 };
